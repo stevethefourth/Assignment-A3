db5066defca05cc703ece3324ec4994b0c8cbc88
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2022-09-25 03:06:33 +1000

    Audio Assets Complete

---
+++ Assets/Scripts/normalBackgroundMusic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class normalBackgroundMusic : MonoBehaviour
+{
+    AudioSource normalMusic;
+    public float startTime = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalMusic = GetComponent<AudioSource>();
+        normalMusic.PlayDelayed(8);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
