1d5f0dc77e3834dcfc7b04ef4479202a7d308140
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2022-10-28 19:11:34 +1100

    Buttons working

--- Assets/Scripts/StartScene/UiManager.cs
+++ Assets/Scripts/StartScene/UiManager.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
-    public Button Level1;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Button button 
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void loadScene1()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadSceneAsync(1);
+        SceneManager.sceneLoaded += OnSceneLoaded;
         
+
+    }
+
+    public void loadScene2()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadSceneAsync(2);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
     }
 }
