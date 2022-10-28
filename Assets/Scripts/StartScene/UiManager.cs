359cbe59bef8dfd6aa6fb5396ead6ecadaeb2acd
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2022-10-28 17:43:16 +1100

    Button Start

---
+++ Assets/Scripts/StartScene/UiManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public Button Level1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button button 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
