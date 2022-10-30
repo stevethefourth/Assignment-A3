757eeee2d8285093a03bf042f65ed82741eab6e8
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2022-10-30 22:10:31 +1100

    cherry spawn done

--- Assets/Scripts/BaseScene/CherryController.cs
+++ Assets/Scripts/BaseScene/CherryController.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CherryController : MonoBehaviour
 {
+
+    public GameObject cherry;
+    private float buffer = 10.0f;
+    private float yValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class CherryController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var val = Random.Range(0.0f,1.0f);
+        if(val < 0.5f)
+        {
+            yValue = 0;
+        }
+        else
+        {
+            yValue = 31;
+        }
+        float current =+ Time.deltaTime;
+        if (buffer < current)
+        {
+            buffer = +current;
+            Instantiate(cherry, new Vector3(Random.Range(5.0f, 30.0f), yValue,0.0f), Quaternion.identity);
+        }
     }
+    
 }
