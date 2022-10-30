bb0a8e39d7805892a0152f8a5b74192a6a9d0125
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2022-10-30 22:33:54 +1100

    Cherry lerping done?

--- Assets/Scripts/BaseScene/CherryController.cs
+++ Assets/Scripts/BaseScene/CherryController.cs
@@ -8,7 +8,8 @@ public class CherryController : MonoBehaviour
 
     public GameObject cherry;
     private float buffer = 10.0f;
-    private float yValue;
+    private float current;
+    private float timeToLerp = 8.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,21 +19,53 @@ public class CherryController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var val = Random.Range(0.0f,1.0f);
-        if(val < 0.5f)
+        
+        current += Time.deltaTime;
+        Debug.Log(current);
+        if (current > buffer)
         {
-            yValue = 0;
+            
+            Instantiate(cherry, new Vector3(Random.Range(5.0f, 30.0f), randomValue(),0.0f), Quaternion.identity);
+            current = current - buffer; 
         }
-        else
+        if(GameObject.FindGameObjectsWithTag("Cherry") != null)
         {
-            yValue = 31;
+            move();
+            offScreen();
         }
-        float current =+ Time.deltaTime;
-        if (buffer < current)
+        
+    }
+    public void offScreen()
+    {
+        if(cherry.transform.position.y == 2.0f)
         {
-            buffer = +current;
-            Instantiate(cherry, new Vector3(Random.Range(5.0f, 30.0f), yValue,0.0f), Quaternion.identity);
+            Destroy(gameObject);
         }
+        if (cherry.transform.position.y == -33.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void move()
+    {
+        if (cherry.transform.position.y == -32 )
+        {
+            cherry.transform.position = Vector3.Lerp(cherry.transform.position, new Vector3(cherry.transform.position.x,2.0f,0.0f), timeToLerp);
+        }
+        else if (cherry.transform.position.y == 1)
+        {
+            cherry.transform.position = Vector3.Lerp(cherry.transform.position, new Vector3(cherry.transform.position.x, -33.0f, 0.0f), timeToLerp);
+        }
+    }
+    public float randomValue()
+    {
+        var val = Random.Range(0.0f, 1.0f);
+        if (val < 0.5f)
+        {
+            return  1;
+        }
+        
+           return  -32;
         
     }
     
