07a475fc0cf1e7f2f2f018fe054df28e91a4bb0e
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2022-10-30 23:08:53 +1100

    cherry working

--- Assets/Scripts/BaseScene/CherryController.cs
+++ Assets/Scripts/BaseScene/CherryController.cs
@@ -7,35 +7,41 @@ public class CherryController : MonoBehaviour
 {
 
     public GameObject cherry;
-    private float buffer = 1.0f;
+    private float buffer = 10.0f;
     private float current;
-    private float timeToLerp = 8.0f;
+    private Tweener tweener;
     private GameObject clone;
+    private float yValue;
+    private float timeToTween = 8.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        tweener = GetComponent<Tweener>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        yValue = randomValue();
         current += Time.deltaTime;
         Debug.Log(current);
         if (current > buffer)
         {
             
-            Instantiate(cherry, new Vector3(Random.Range(5.0f, 30.0f), randomValue(),0.0f), Quaternion.identity);
+            Instantiate(cherry, new Vector3(Random.Range(5.0f, 30.0f), yValue,0.0f), Quaternion.identity);
             current = current - buffer; 
         }
         clone = GameObject.FindGameObjectWithTag("Cherry");
-        if (GameObject.FindGameObjectWithTag("Cherry") != null)
+        if (GameObject.FindGameObjectWithTag("Cherry") != null && !tweener.TweenExists(clone.transform))
         {
             Debug.Log("working");
             move();
-            if (clone.transform.position.y == -33.0f || clone.transform.position.y == 2.0f)
-                Destroy(cherry);
+            if(clone.transform.position.y > 7.50f|| clone.transform.position.y < -38.50f)
+            {
+                Destroy(clone);
+            }
+            
         }
         
     }
@@ -44,11 +50,13 @@ public class CherryController : MonoBehaviour
     {
         if (clone.transform.position.y == -32 )
         {
-            clone.transform.position = Vector3.Lerp(clone.transform.position, new Vector3(clone.transform.position.x,2.0f,0.0f), timeToLerp);
+            tweener.AddTween(clone.transform, clone.transform.position, new Vector3(clone.transform.position.x, 8.0f, 0.0f), timeToTween);
+           
         }
         else if (clone.transform.position.y == 1)
         {
-            clone.transform.position = Vector3.Lerp(clone.transform.position, new Vector3(clone.transform.position.x, -33.0f, 0.0f), timeToLerp);
+
+            tweener.AddTween(clone.transform, clone.transform.position, new Vector3(clone.transform.position.x, -39.0f, 0.0f), timeToTween);
         }
     }
     public float randomValue()
